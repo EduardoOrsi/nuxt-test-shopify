0b3b26860a4e4fa37a4e5b9a9c057968dbf70bf8
Eduardo Orsi <eduardo.orsi@phddobrasil.com>
2026-03-24 17:23:00 -0300

    fix: error 500

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,23 +1,33 @@
 <script setup lang="ts">
 const storefront = useStorefront();
 
-const { data } = await storefront.request(`#graphql
-    query FetchProducts($first: Int) {
-        products(first: $first) {
-            nodes {
-                id
-                title
-                description
-            }
-        }
-    }
-`, {
-	variables: {
-		first: 10,
-	},
-});
+const { data, error } = await useAsyncData("products", () =>
+	storefront.request(`#graphql
+		query FetchProducts($first: Int) {
+			products(first: $first) {
+				nodes {
+					id
+					title
+					description
+				}
+			}
+		}
+	`, {
+		variables: {
+			first: 10,
+		},
+	}),
+);
 </script>
 
 <template>
-	<pre>{{ data?.products }}</pre>
+	<div class="p-8">
+		<p
+			v-if="error"
+			class="text-red-400"
+		>
+			Erro ao carregar produtos: {{ error.message }}
+		</p>
+		<pre v-else>{{ data?.data?.products }}</pre>
+	</div>
 </template>
